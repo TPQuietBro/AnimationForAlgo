e49a4f577bee3b74b800be2b7a9938184bd39558
tangpeng <allentp@163.com>
2018-10-09 11:49:12 +0800

    update radix

--- AnimationsForAlgo/Sort/Radix/RadixViewController.m
+++ AnimationsForAlgo/Sort/Radix/RadixViewController.m
@@ -8,7 +8,9 @@
 
 #import "RadixViewController.h"
 #import "TPFrame.h"
-static NSInteger colSize = 20;
+static NSInteger colSize = 9;
+static CGFloat margin = 7.5;
+static CGFloat colMargin = 5;
 #define firstLabel self.labels.firstObject
 
 @interface RadixViewController ()
@@ -16,6 +18,7 @@ static NSInteger colSize = 20;
 @property (nonatomic, assign) NSInteger loops;
 @property (nonatomic, strong) NSArray *baseNumbers;
 @property (nonatomic, strong) NSMutableArray *tempArray;
+@property (nonatomic, strong) NSMutableArray *baseArray;
 @end
 
 @implementation RadixViewController
@@ -26,7 +29,7 @@ static NSInteger colSize = 20;
     self.maxNumber = [self maxNumber];
     self.loops = [self loopsWithNumber:self.maxNumber];
     self.baseNumbers = @[@"0",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9"];
-    
+    self.baseArray = [@[] mutableCopy];
     [self initBaseNumberLabels];
     [self addSepLine];
     
@@ -35,6 +38,7 @@ static NSInteger colSize = 20;
 }
 
 - (void)beginAnimation{
+    
     if (self.j == self.loops) {
         [self fireTimer];
         [self afterSorted];
@@ -44,6 +48,8 @@ static NSInteger colSize = 20;
     if (self.i == self.examples.count) {
         
         self.j ++;
+        self.i = 0;
+        self.k = 0;
         return;
     }
     
@@ -55,9 +61,20 @@ static NSInteger colSize = 20;
     // 找到个位,十位......
     NSInteger rowIndex = [self positionWithNumber:[self.examples[self.i] integerValue] loop:self.j];
     if ([self.tempArray[rowIndex][self.k] length] == 0) {
-        [self.tempArray[rowIndex] replaceObjectAtIndex:self.k withObject:self.examples[self.i]];
-        self.i ++;
-        self.k = 0;
+        UILabel *label = self.labels[self.i];
+        CGRect originFrame = label.frame;
+        UILabel *baseLabel = self.baseArray[rowIndex];
+        [UIView animateWithDuration:Duration animations:^{
+            [self fireTimer];
+            label.x = baseLabel.x + width + margin + self.k * (width + colMargin);
+            label.y = baseLabel.y;
+        } completion:^(BOOL finished) {
+            [self addCpLabelWithLabel:label originFrame:originFrame];
+            [self.tempArray[rowIndex] replaceObjectAtIndex:self.k withObject:self.examples[self.i]];
+            self.i ++;
+            self.k = 0;
+            [self startTimer];
+        }];
     } else {
         self.k ++;
     }
@@ -87,25 +104,35 @@ static NSInteger colSize = 20;
         .tp_systemTextColor(ENRedColor).tp_bgColor([UIColor whiteColor]);
         label.frame = CGRectMake([firstLabel x], CGRectGetMaxY([firstLabel frame]) + topMargin + i * (height  + labelMargin), width, height);
         [self.view addSubview:label];
+        [self.baseArray addObject:label];
     }
     
     CGFloat rowTopMargin = 15;
-    for (NSInteger j = 0; j < self.loops; ++j) {
+    for (NSInteger j = 0; j < colSize; ++j) {
         UILabel *label = [[UILabel alloc] init];
         label.tp_text([NSString stringWithFormat:@"%zd",j]).tp_font(@12).tp_alignment(ENTextAlighmentCenter)
         .tp_bgColor([UIColor whiteColor]);
-        label.frame = CGRectMake([firstLabel x] + width + labelMargin + (width + labelMargin) * j, CGRectGetMaxY([firstLabel frame]) + rowTopMargin , width, height);
+        label.frame = CGRectMake([firstLabel x] + width + colMargin + (width + colMargin) * j, CGRectGetMaxY([firstLabel frame]) + rowTopMargin , width, height);
         [self.view addSubview:label];
     }
     
 }
 
 - (void)addSepLine{
-    CGFloat margin = 7.5;
+    
     UILabel *sepLabel = [[UILabel alloc] init];
     sepLabel.tp_bgColor([UIColor orangeColor]);
     sepLabel.frame = CGRectMake([firstLabel x], CGRectGetMaxY([firstLabel frame]) + margin, self.view.width - 2 * [firstLabel x], 1);
     [self.view addSubview:sepLabel];
+}
+
+- (void)addCpLabelWithLabel:(UILabel *)label originFrame:(CGRect)originFrame{
+    UILabel *cpLabel = [[UILabel alloc] init];
+    cpLabel.tp_text(label.text).tp_font(label.font).
+    tp_alignment(ENTextAlighmentCenter).tp_bgColor(label.backgroundColor);
+    cpLabel.frame = label.frame;
+    [self.view addSubview:cpLabel];
+    label.frame = originFrame;
 }
 
 - (NSInteger)loopsWithNumber:(NSInteger)number{
